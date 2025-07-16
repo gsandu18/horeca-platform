dbf4a6cee64e2ab8df906ee4fed344d212458813
gsandu18 <gsandu614@gmail.com>
2025-07-16 23:35:05 +0200

    Update document.js

--- document.js
+++ document.js
@@ -2,3 +2,13 @@ router.get('/all', async (req, res) => {
   const allDocs = await require('../models/Document').find();
   res.json(allDocs);
 });
+const documentSchema = new mongoose.Schema({
+  firmaId: String,
+  nume: String,
+  path: String,
+  status: { type: String, enum: ['in asteptare', 'aprobat', 'respins'], default: 'in asteptare' },
+  comentariu: String,
+  data_incarcare: { type: Date, default: Date.now },
+  actualizat_de: String, // email sau ID admin
+  data_validare: Date
+});
