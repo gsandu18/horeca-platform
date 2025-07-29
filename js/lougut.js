18f27130a64244d2693ff69feb2614a89ae6f5d6
gsandu18 <gsandu614@gmail.com>
2025-07-29 22:28:39 +0200

    Create lougut.js

---
+++ js/lougut.js
@@ -0,0 +1,5 @@
+function logout() {
+  // Aici poți adăuga logica de ștergere token/session dacă folosești backend
+  alert("Te-ai delogat cu succes!");
+  window.location.href = "login.html"; // redirecționează către pagina de login
+}
